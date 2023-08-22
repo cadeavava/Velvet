0b36b301771032c6131322116d4d27d21730c974
cadeavava <137566635+cadeavava@users.noreply.github.com>
2023-08-22 08:06:51 -0300

    carrossel.js

--- carrossel.js
+++ carrossel.js
@@ -1,24 +1,27 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-nextImage();
-}, 5000)
+// Adicione a classe 'active' à primeira imagem
+document.querySelector(".slide:nth-child(1)").classList.add("active");
 
-function nextImage(){
+setInterval(function () {
+  nextImage();
+}, 5000);
+
+function nextImage() {
   count++;
-  if(count>4){
+  if (count > 4) {
     count = 1;
   }
 
- // Remove a classe 'active' de todas as imagens
+  // Remova a classe 'active' de todas as imagens
   document.querySelectorAll('.slide').forEach(slide => {
     slide.classList.remove('active');
   });
 
-  // Adiciona a classe 'active' à imagem atual
+  // Adicione a classe 'active' à imagem atual
   document.querySelector(".slide:nth-child(" + count + ")").classList.add("active");
 
-  // Atualiza o botão de navegação (se necessário)
+  // Atualize o botão de navegação (se necessário)
   document.getElementById("radio" + count).checked = true;
 }
